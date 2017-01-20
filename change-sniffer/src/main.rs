20bf739201abcd704c5a3ba9d66d09acd3e8f340
phasedchirp <phasedchirp2718@gmail.com>
2017-01-19 17:42:32 -0800

    starting to actually do stuff with results

--- change-sniffer/src/main.rs
+++ change-sniffer/src/main.rs
@@ -1,13 +1,23 @@
 extern crate hyper;
 
-use hyper::client::Client;
+use hyper::client::{Client,Response};
+// use hyper::client::response::Response;
+use hyper::status::StatusCode;
 
+use std::io::Read;
 use std::process::Command;
 use std::env::args;
 
 fn main() {
     let inputs: Vec<String> = args().collect(); // url, frequency
     let client = Client::new();
-    let result = client.get(&inputs[1]).send().unwrap();
-    println!("{:?}", result.status);
+    let mut result = client.get(&inputs[1]).send().unwrap();
+    match result.status {
+        StatusCode::Ok => {
+            let mut buf = String::new();
+            result.read_to_string(&mut buf);
+            println!("{:?}", buf);
+        },
+        _ => println!("Some other status code")
+    }
 }
