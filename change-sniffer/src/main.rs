864e65667d391ce1f91bbbfad5b0462aa315438a
phasedchirp <sean.martin@nyu.edu>
2017-01-21 14:28:39 -0800

    Now with slightly more functional initialization

--- change-sniffer/src/main.rs
+++ change-sniffer/src/main.rs
@@ -1,7 +1,7 @@
 extern crate hyper;
 extern crate chrono;
 
-use hyper::client::{Client,Response};
+use hyper::client::Client;
 use hyper::status::StatusCode;
 
 use chrono::UTC;
@@ -9,12 +9,12 @@ use chrono::UTC;
 use std::io::{Read,Write};
 use std::process::Command;
 use std::env::args;
-use std::fs::{metadata,OpenOptions,File,DirBuilder};
+use std::fs::{metadata,OpenOptions,File};
 use std::collections::HashMap;
 
 fn setup() {
     Command::new("mkdir").arg("../tracking").output().expect("mkdir failed");
-    Command::new("touch").arg("../.config").output().expect("config creation failed");
+    Command::new("touch").arg("../config").output().expect("config creation failed");
     Command::new("touch").arg("../fail-log").output().expect("log creation failed");
 }
 
@@ -25,16 +25,17 @@ fn read_config() -> HashMap<String,String> {
         Ok(mut file) => {
 
             let mut contents = String::new();
-            file.read_to_string(&mut contents);
+            file.read_to_string(&mut contents).unwrap();
             let lines: Vec<&str> = contents.split("\n").collect();
-            for s in lines {
-                let info: Vec<String> = s.split_whitespace()
-                            .map(|x| x.to_string()).collect();
-                config.insert(info[0].clone(),info[1].clone());
+            if lines != [""] {
+                for s in lines {
+                    let info: Vec<String> = s.split_whitespace()
+                                .map(|x| x.to_string()).collect();
+                    config.insert(info[0].clone(),info[1].clone());
+                }
             }
         },
         Err(e) => {
-            // let mut config = HashMap::new();
             panic!("Failed to open config file: {:?}",e);
         }
     }
@@ -43,21 +44,17 @@ fn read_config() -> HashMap<String,String> {
 
 fn update_config(name: &str, addr: &str) {
     let mut f = OpenOptions::new().write(true).append(true).create(true)
-                .open("config").unwrap();
-    f.write_all(format!("{} {}",name,addr).as_bytes());
+                .open("../config").unwrap();
+    f.write_all(format!("{} {}",name,addr).as_bytes()).unwrap();
 }
 
-fn initialize(name: &str, addr: &str) {
-    match metadata(&format!("../tracking/{}/",name)) {
-        Ok(_) => panic!("That name is already in use"),
-        Err(_) => {
-            let git_path = format!("../tracking/{}/",name);
-            let c1 = Command::new("mkdir").arg(&format!("../tracking/{}",name))
-                    .output().expect("mkdir failed");
-            let c2 = Command::new("git").args(&["-C",&git_path,"init"]).output()
-                    .expect("git init failed");
-        }
-    }
+fn initialize(name: &str) {
+    let git_path = format!("../tracking/{}/",name);
+    let _ = Command::new("mkdir").arg(&git_path).output().expect("mkdir failed");
+    let _ = Command::new("git").args(&["-C",&git_path,"init"]).output()
+            .expect("git init failed");
+    let _ = Command::new("touch").arg("../result.txt").output().expect("tracking file fail");
+
 }
 
 fn commit_changes(name: &str) -> String {
@@ -75,7 +72,8 @@ fn commit_changes(name: &str) -> String {
 }
 
 fn get_changes(name: &str) -> Vec<u8> {
-    let hashes = Command::new("git").args(&["log","-2","--pretty=format:\"%H\""])
+    let dir = format!("../tracking/{}",name);
+    let hashes = Command::new("git").args(&["-C",&dir,"log","-2","--pretty=format:\"%H\""])
                  .output().expect("git log failed");
     let commits: Vec<String> = String::from_utf8_lossy(&hashes.stdout)
                              .split_whitespace().map(|s| s.to_string())
@@ -85,28 +83,28 @@ fn get_changes(name: &str) -> Vec<u8> {
     diffs.stdout
 }
 
-fn fetch(name: &str) {
+fn fetch(name: &str, addr: &str) {
+    let path = format!("../tracking/{}/result.txt",name);
     let client = Client::new();
-    let mut result = client.get("addr").send().unwrap();
+    let mut result = client.get(addr).send().unwrap();
     match result.status {
         StatusCode::Ok => {
             let mut buff_old = String::new();
             let mut buff_new = String::new();
-            let mut file = OpenOptions::new().write(true).truncate(true)
-                        .create(true).open("result.txt").unwrap();
-
-            file.read_to_string(&mut buff_old);
-            result.read_to_string(&mut buff_new);
+            let mut file = OpenOptions::new().read(true).write(true).truncate(true)
+                        .create(true).open(&path).unwrap();
+            file.read_to_string(&mut buff_old).unwrap();
+            result.read_to_string(&mut buff_new).unwrap();
             // compare old and new responses, write if changes have occurred:
             if buff_old != buff_new {
-                file.write_all(buff_new.as_bytes());
+                file.write_all(buff_new.as_bytes()).unwrap();
             }
         },
         x => {
             let mut file = OpenOptions::new().write(true).append(true)
                         .create(true).open("fail-log").unwrap();
             let msg = format!("Attempt to fetch {} failed with status code {}","addr",x);
-            file.write_all(msg.as_bytes());
+            file.write_all(msg.as_bytes()).unwrap();
         }
     }
 }
@@ -118,19 +116,26 @@ fn main() {
     match &*inputs[1] {
         "setup" => setup(),
         "add" => {
-            let mut config = read_config();
+            let config = read_config();
             match config.get(&inputs[2]) {
                 Some(addr) => println!("The name {} is already in use for {}",&inputs[2],&addr),
                 None => {
-                    initialize(&inputs[2],&inputs[3]);
-                    fetch(&inputs[2]);
+                    initialize(&inputs[2]);
+                    fetch(&inputs[2],&inputs[3]);
+                    update_config(&inputs[2],&inputs[3]);
                 }
             }
         },
         "update" => {
-            fetch(&inputs[2]);
-            let res = commit_changes(&inputs[2]);
-            println!("{}",res);
+            let config = read_config();
+            match config.get(&inputs[2]) {
+                Some(addr) => {
+                    fetch(&inputs[2],&addr);
+                    let res = commit_changes(&inputs[2]);
+                    println!("{}",res);
+                },
+                None => println!("That name isn't being tracked")
+            }
         },
         "diffs" => {
             get_changes(&inputs[2]);
